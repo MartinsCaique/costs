b1724d7b91624bbe20df9728cd6141e3a21ceed1
MartinsCaique <caiquety1@gmail.com>
2024-09-24 06:48:44 -0300

    Criando o db.json que é um banco de dados e conectando ele com uma parte do nosso código.

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,3 +1,6 @@
+// Extensões do react
+import { useEffect, useState } from 'react';
+
 // Components
 import Input from '../form/Input';
 import Select from '../form/Select';
@@ -7,11 +10,28 @@ import SubmitButton from '../form/SubmitButton';
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=> {
+        fetch('http://localhost:5000/categories', {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((resp)=> resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err)=> console.log(err))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do projeto' />
             <Input type='number' text='Orçamento do Projeto' name='budget' placeholder='Insira o orçamento total' />
-            <Select name='categoryId' text='Selecione a categoria' />
+            <Select name='categoryId' text='Selecione a categoria' options={categories} />
             <SubmitButton text={btnText} />
         </form>
     );
