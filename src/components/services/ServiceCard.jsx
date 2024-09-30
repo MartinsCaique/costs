6cb9c6368f2a06f0e3aa0b6a6c508b9648a34017
MartinsCaique <caiquety1@gmail.com>
2024-09-30 09:55:20 -0300

    Removendo Serviços e Terminando o projeto

--- src/components/services/ServiceCard.jsx
+++ src/components/services/ServiceCard.jsx
@@ -2,9 +2,12 @@
 import { BsFillTrashFill } from 'react-icons/bs';
 import styles from '../project/ProjectCard.module.css'
 
-function ServiceCard({id, name, cost, description, handleRemove}) {
+function ServiceCard({ id, name, cost, description, handleRemove }) {
 
-    const remove = (e)=> {}
+    const remove = (e) => {
+        e.preventDefault()
+        handleRemove(id, cost)
+    }
 
     return (
         <div className={styles.projectCard}>
